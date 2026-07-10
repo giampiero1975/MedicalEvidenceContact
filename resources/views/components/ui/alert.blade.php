6164af0d21e8defe915e03969863bccc24ea5dc6
giampiero1975 <giampiero.digregorio@gmail.com>
2026-07-10 17:31:06 +0200

    Add reusable UI alert component

---
+++ resources/views/components/ui/alert.blade.php
@@ -0,0 +1,22 @@
+@props([
+    'variant' => 'info',
+    'title' => null,
+])
+
+@php
+    $variants = [
+        'info' => 'border-sky-200 bg-sky-50 text-sky-900',
+        'success' => 'border-emerald-200 bg-emerald-50 text-emerald-900',
+        'warning' => 'border-amber-200 bg-amber-50 text-amber-900',
+        'danger' => 'border-rose-200 bg-rose-50 text-rose-900',
+    ];
+@endphp
+
+<div role="alert" {{ $attributes->class(['rounded-xl border px-4 py-3 text-sm', $variants[$variant] ?? $variants['info']]) }}>
+    @if ($title)
+        <p class="font-semibold">{{ $title }}</p>
+    @endif
+    <div @class(['mt-1' => $title])>
+        {{ $slot }}
+    </div>
+</div>
