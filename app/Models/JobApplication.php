9ac90e5feb50d1ffdeebdcb15fa62b0f6b357b56
giampiero1975 <giampiero.digregorio@gmail.com>
2026-07-14 14:32:18 +0200

    Add candidate pipeline statuses

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -10,11 +10,40 @@ class JobApplication extends Model
 {
     use HasFactory;
 
+    public const STATUS_RECEIVED = 'inviata';
+    public const STATUS_REVIEW = 'in_valutazione';
+    public const STATUS_INTERVIEW_SCHEDULED = 'colloquio_programmato';
+    public const STATUS_INTERVIEW_COMPLETED = 'colloquio_effettuato';
+    public const STATUS_SUITABLE = 'idoneo';
+    public const STATUS_HIRED = 'assunto';
+    public const STATUS_REJECTED = 'non_idoneo';
+    public const STATUS_WITHDRAWN = 'ritirata';
+
     protected $fillable = [
         'job_posting_id',
         'user_id',
         'status',
     ];
+
+    /** @return array<string, string> */
+    public static function statusOptions(): array
+    {
+        return [
+            self::STATUS_RECEIVED => 'Ricevuta',
+            self::STATUS_REVIEW => 'In valutazione',
+            self::STATUS_INTERVIEW_SCHEDULED => 'Colloquio programmato',
+            self::STATUS_INTERVIEW_COMPLETED => 'Colloquio effettuato',
+            self::STATUS_SUITABLE => 'Idoneo',
+            self::STATUS_HIRED => 'Assunto',
+            self::STATUS_REJECTED => 'Non idoneo',
+            self::STATUS_WITHDRAWN => 'Ritirata',
+        ];
+    }
+
+    public function statusLabel(): string
+    {
+        return self::statusOptions()[$this->status] ?? ucfirst(str_replace('_', ' ', $this->status));
+    }
 
     public function jobPosting(): BelongsTo
     {
