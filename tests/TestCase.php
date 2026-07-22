b8acc59167779893d563a15d42ba543b7e7cc50a
giampiero1975 <giampiero.digregorio@gmail.com>
2026-07-22 18:01:31 +0200

    Seed business types in feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use Database\Seeders\BusinessTypeSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Schema;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (Schema::hasTable('business_types')) {
+            $this->seed(BusinessTypeSeeder::class);
+        }
+    }
 }
