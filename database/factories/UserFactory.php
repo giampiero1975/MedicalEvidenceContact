f2917b358f6c872a12415407fa664832601e8c9a
giampiero1975 <giampiero.digregorio@gmail.com>
2026-09-09 12:23:45 +0200

    Fix user factory UUID under Event::fake

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
         $lastName = fake()->lastName();
 
         return [
+            'uuid' => (string) Str::uuid(),
             'name' => $firstName.' '.$lastName,
             'first_name' => $firstName,
             'last_name' => $lastName,
